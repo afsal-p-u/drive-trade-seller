9bc4dd15c02dedd31b87b97f9c0052809db0f67a
afsal-p-u <afsalpu07@gmail.com>
2024-12-03 10:33:12 +0530

    day 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,40 @@
 import { CiSearch } from "react-icons/ci";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-[10vh] px-[100px] flex items-center justify-between">
-      <h2>DriveTrade</h2>
+    <div className="w-full h-[10vh] px-[100px] flex items-center justify-between shadow-sm">
+      <h2 className="font-semibold text-xl">Drive<span className="text-blue-500">Trade</span></h2>
 
-      <div className="flex items-center gap-2 bg-gray-100 px-5 py-1 rounded-md">
-        <CiSearch className="cursor-pointer" />
-        <div className="">
+      <div className="flex items-center gap-3">
+        <div className="flex items-center gap-2 bg-gray-100 px-5 py-2 rounded-md">
+          <CiSearch className="cursor-pointer" />
           <input
             type="search"
             name=""
             id=""
-            className="border-nne outline-none bg-transparent text-sm"
+            className="border-nne outline-none bg-transparent text-xs"
             placeholder="Search here"
           />
         </div>
 
-        <div className=""></div>
+        <div className="w-[30px] h-[30px] rounded-md flex items-center cursor-pointer justify-center 
+          bg-gray-100"
+        >
+          <IoMdNotificationsOutline className="text-yellow-500" />
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <div className="">
+            <img src="" alt="" />
+          </div>
+          <div className="">
+            <h6 className="text-sm font-medium">John Dao</h6>
+            <p className="text-xs text-gray-400">Admin</p>
+          </div>
+          <IoIosArrowDown />
+        </div>
       </div>
     </div>
   );
